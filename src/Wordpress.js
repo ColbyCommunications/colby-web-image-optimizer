919a2ceaf8c37c99b14ed76672fa579714098319
Brandon K. Waltz <bwaltz6@gmail.com>
2023-01-10 13:27:17 -0500

    remove reset button on wp page

--- src/Wordpress.js
+++ src/Wordpress.js
@@ -245,13 +245,6 @@ class Wordpress extends React.Component {
                                                     </>
                                                 )}
                                             </button>
-                                            <button
-                                                type="button"
-                                                className="btn btn-secondary"
-                                                onClick={() => window.location.reload()}
-                                            >
-                                                Reset
-                                            </button>
                                         </div>
                                     </div>
                                 </>
